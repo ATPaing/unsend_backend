accebc82fe8326349a833cc61c2a9f64e64c4122
ATPaing <aungthetpaing.2001@gmail.com>
2026-08-18 15:10:28 +0100

    server listen changed

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import env from './config/env.js';
 import app from './app.js';
 
-app.listen(env.port, () => {
-  console.log(`Server listening on http://localhost:${env.port}`);
-});
+app.listen(env.port, '127.0.0.1', () => {
+    console.log(`Server listening on http://127.0.0.1:${env.port}`);
+  });
